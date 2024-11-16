787045075d31926f8955414b9c0d92f9a372e841
Alishbakhan313201 <Alishbakhan817a@gmail.com>
2024-11-16 20:59:32 +0500

    Add files via upload

---
+++ mid - project.py
@@ -0,0 +1,44 @@
+print (" recipe program ")
+
+# ingreadient stored in variable
+flour = 2           # cups
+bakingsoda = 0.5    # tsps
+salt =salt = 0.5    # tsps
+butter = 0.75       # cups
+suggar = 0.5        # cups
+eggs = 1            # egg
+choclatechip = 2    # cups
+
+print( "" )
+print( "choclate chip cookies recipe" )
+print( "" )
+print( flour, " cups of flour" )
+print( bakingsoda, " tsps of bakingsoda" )
+print( salt, " tsps of salt" )
+print( butter, " cups of butter" )
+print( suggar, " cups of suggar" )
+print( eggs, " eggs" )
+print( choclatechip, " cups of choclatechips" )
+
+print( "" )
+batchQuanity = float( input( "how many batches would you like to make? :(0.5 for half, 2 for double: " ))
+
+flour = 2 * batchQuanity
+bakingsoda = 0.5 * batchQuanity
+salt = 0.5 * batchQuanity
+butter = 0.75 * batchQuanity
+suggar = 0.5 * batchQuanity
+egg = 1 * batchQuanity
+choclatechip = 2 * batchQuanity
+
+print( "" )
+print( "choclatechip cookies recipe" )
+print( "" )
+print( flour, " cups of flour" )
+print( bakingsoda, " tsps of bakingsoda" )
+print( salt, " tsps of salt" )
+print( butter, " cups of butter" )
+print( suggar, " cups of suggar" )
+print( egg, "eggs" )
+print( choclatechip, "cups of choclatechip" )
+
